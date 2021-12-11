ce861f77304ea3323e801d77cce16f02d7b32013
Vikram Khandelwal <vikram.a.khandelwal@gmail.com>
2021-12-11 11:53:49 -0800

    Commented on the Practice object

--- ServePlusDraft/Model/Practice+CoreDataClass.swift
+++ ServePlusDraft/Model/Practice+CoreDataClass.swift
@@ -4,7 +4,9 @@
 //
 //  Created by Vikram Khandelwal on 8/5/21.
 //
-//
+//  Base class for storing a "practice", which is
+//  a collection of scored serves taken from one
+//  continuous practice session.
 
 import Foundation
 import CoreData
--- ServePlusDraft/Model/Practice+CoreDataProperties.swift
+++ ServePlusDraft/Model/Practice+CoreDataProperties.swift
@@ -4,22 +4,32 @@
 //
 //  Created by Vikram Khandelwal on 8/5/21.
 //
-//
+//  Extension of the Practice class that details
+//  the variables that define a single Practice
+//  entity.
 
 import Foundation
 import CoreData
 
 
 extension Practice {
-
+    
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Practice> {
         return NSFetchRequest<Practice>(entityName: "Practice")
     }
 
-    @NSManaged public var date: Date?
-    @NSManaged public var urls: [URL]?
-    @NSManaged public var vectors: [[Double]]?
-    @NSManaged public var timestamps: [[Int]]?
+    @NSManaged public var date: Date?           // date of the practice session
+    @NSManaged public var urls: [URL]?          // local URLs of the individual serves'
+                                                // videos
+    @NSManaged public var vectors: [[Double]]?  // serve scores for each individual serve.
+                                                // Each vector contains 8 elements for
+                                                // each of the scoring categories.
+    @NSManaged public var timestamps: [[Int]]?  // starting and ending frames for each
+                                                // serve in a continuous practice video.
+                                                // Instead of creating a new video for
+                                                // every serves, this array helps to
+                                                // split a continuous practice video
+                                                // into individual serves on the spot.
 
 }
 
