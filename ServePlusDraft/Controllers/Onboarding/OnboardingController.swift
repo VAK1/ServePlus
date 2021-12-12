ae8191e096e22f53992a783b74e17e68ac86d3a2
Vikram Khandelwal <vikram.a.khandelwal@gmail.com>
2021-12-11 18:32:49 -0800

    Commented the Onboarding Files

--- ServePlusDraft/Controllers/Onboarding/OnboardingController.swift
+++ ServePlusDraft/Controllers/Onboarding/OnboardingController.swift
@@ -4,21 +4,24 @@
 //
 //  Created by Vikram Khandelwal on 10/5/21.
 //
+//  Page View Controller to manage the navigation and aesthetics of the tutorial
 
 import Foundation
 import UIKit
 
 class OnboardingController: UIPageViewController {
-
+    
+    
+    // Initialize container for the tutorial's pages, or sections
     var pages = [UIViewController]()
 
-    // external controls
+    // External controls
     let skipButton = UIButton()
     let nextButton = UIButton()
     let pageControl = UIPageControl()
     let initialPage = 0
 
-    // animations    
+    // Animations
     var skipButtonTopAnchor: NSLayoutConstraint?
     var nextButtonTopAnchor: NSLayoutConstraint?
     var pageControlBottomAnchor: NSLayoutConstraint?
@@ -26,6 +29,7 @@ class OnboardingController: UIPageViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Setup, style and layout the tutorial
         setup()
         style()
         layout()
@@ -35,59 +39,125 @@ class OnboardingController: UIPageViewController {
 extension OnboardingController {
     
     func setup() {
+        
+        /* Initialize the pages of the overarching Onboarding Controller. Each
+           page will be a subcontroller, called an OnboardingViewController. */
+        
+        // Set the data source and delegate
         dataSource = self
         delegate = self
         
+        
+        /* If the little dots at the bottom of the page view controller are tapped,
+           then navigate to the relevant page */
         pageControl.addTarget(self, action: #selector(pageControlTapped(_:)), for: .valueChanged)
 
+        
+        // Initialize the necessary pages
         let page1 = OnboardingViewController(imageName: "OnboardingImage1",
                                              titleText: "ServePlus brings AI to you",
                                              subtitleText: "AI for your tennis serve. It's time to get excited!",
-                                             imageSize: 0.8 )
+                                             imageSize: 0.8 ) // Welcome
         let page2 = OnboardingViewController(imageName: "OnboardingImage2",
                                              titleText: "Get your serves in",
                                              subtitleText: "Start with recording a video of your serves, or upload a previous clip.",
-                                             imageSize: 1.0)
+                                             imageSize: 1.0) // How to use
         let page3 = OnboardingViewController(imageName: "OnboardingImage3",
                                              titleText: "Improve",
                                              subtitleText: "Review feedback in real time through the \"Review\" tab.",
-                                             imageSize: 1.1)
+                                             imageSize: 1.1) // How to review
         let page4 = OnboardingViewController(imageName: "OnboardingImage4",
                                              titleText: "Track your growth",
                                              subtitleText: "Monitor progress graphs through the \"Analyze\" tab.",
-                                             imageSize: 1.1)
+                                             imageSize: 1.1) // How to grow
         
+        
+        // Add the pages to the reference array
         pages.append(page1)
         pages.append(page2)
         pages.append(page3)
         pages.append(page4)
         
+        
+        // Set the pages to be displayed in a forward scrolling fashion
         setViewControllers([pages[initialPage]], direction: .forward, animated: true, completion: nil)
     }
     
     func style() {
+        
+        /* Add some style to the Page Control */
+        
+        
+        // parameter to make the contraints apply properly
         pageControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        // Set the current page dot on the page control to black
         pageControl.currentPageIndicatorTintColor = .black
+        
+        
+        // Set all other page dots to gray
         pageControl.pageIndicatorTintColor = .systemGray2
+        
+        
+        // Set the number of page dots to the number of pages
         pageControl.numberOfPages = pages.count
+        
+        
+        // Set the current page dot to the first page
         pageControl.currentPage = initialPage
 
+        
+        
+        
+        /* Add some style to the Finish button*/
+        
+        // parameter to make the contraints apply properly
         skipButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        // give the Finish button a blue color
         skipButton.setTitleColor(.systemBlue, for: .normal)
+        
+        
+        // give the Finish button a title of "Finish"
         skipButton.setTitle("Finish", for: .normal)
+        
+        
+        // Signal that the skip button was tapped and act accordingly
         skipButton.addTarget(self, action: #selector(skipTapped(_:)), for: .primaryActionTriggered)
-
+        
+        
+        
+        
+        /* Add some style to the Next button*/
+        
+        // parameter to make the contraints apply properly
         nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        // give the Next button a blue color
         nextButton.setTitleColor(.systemBlue, for: .normal)
+        
+        
+        // give the Next button a title of "Next"
         nextButton.setTitle("Next", for: .normal)
+        
+        
+        // Signal that the skip button was tapped and act accordingly
         nextButton.addTarget(self, action: #selector(nextTapped(_:)), for: .primaryActionTriggered)
     }
     
     func layout() {
+        
+        // Add the page control dots and navigation buttons
         view.addSubview(pageControl)
         view.addSubview(nextButton)
         view.addSubview(skipButton)
         
+        
+        /* Position the page control at the bottom and the control buttons
+            on the top corners */
         NSLayoutConstraint.activate([
             pageControl.widthAnchor.constraint(equalTo: view.widthAnchor),
             pageControl.heightAnchor.constraint(equalToConstant: 20),
@@ -98,7 +168,9 @@ extension OnboardingController {
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: nextButton.trailingAnchor, multiplier: 2),
         ])
         
-        // for animations
+        
+        /* Keep track of the button positions in case animations take
+           the buttons elsewhere */
         skipButtonTopAnchor = skipButton.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 2)
         nextButtonTopAnchor = nextButton.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 2)
         pageControlBottomAnchor = view.bottomAnchor.constraint(equalToSystemSpacingBelow: pageControl.bottomAnchor, multiplier: 2)
@@ -109,107 +181,168 @@ extension OnboardingController {
     }
 }
 
-// MARK: - DataSource
 extension OnboardingController: UIPageViewControllerDataSource {
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        /* If the user swipes to the previous page, go there */
 
+        // Check that the page exists
         guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
         
+        
+        // Only go left if the current page isn't the first page
         if currentIndex != 0 {
-            return pages[currentIndex - 1]  // go previous
+            return pages[currentIndex - 1]
         }
         return nil
     }
         
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         
+        /* If the user swipes to the next page, go there */
+        
+        // Check that the page exists
         guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
 
+        
+        // Only go right if the current page isn't th elast page
         if currentIndex < pages.count - 1 {
-            return pages[currentIndex + 1]  // go next
-        } else {
-            return nil
+            return pages[currentIndex + 1]
         }
+        return nil
     }
 }
 
-// MARK: - Delegates
 extension OnboardingController: UIPageViewControllerDelegate {
     
-    // How we keep our pageControl in sync with viewControllers
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
         
+        /* Sync the page control dots with the actual pages being displayed*/
+        
+        
+        // Fetch the pages
         guard let viewControllers = pageViewController.viewControllers else { return }
+        
+        
+        // Get the index of the current page
         guard let currentIndex = pages.firstIndex(of: viewControllers[0]) else { return }
         
+        
+        // Let the page control know what the current page is
         pageControl.currentPage = currentIndex
+        
+        
+        // Animate the changes if necessary
         animateControlsIfNeeded()
     }
     
     private func animateControlsIfNeeded() {
+        
+        /* If the tutorial is on the last page, hide the controls. Otherwise,
+           put the controls back where they belong. */
+        
+        // Boolean that knows whether the current page is the last one.
         let lastPage = pageControl.currentPage == pages.count - 1
         
+        
+        // Hide the "next" button if this is the last page
         if lastPage {
             hideControls()
-        } else {
+        }
+        
+        // show the "next" button if this is not the last page
+        else {
             showControls()
         }
 
+        
+        /* This PropertyAnimator will ensure that whenever controls are hidden
+           or unhidden, they will glide seamlessly back onto display */
         UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5, delay: 0, options: [.curveEaseOut], animations: {
             self.view.layoutIfNeeded()
         }, completion: nil)
     }
     
     private func hideControls() {
+        
+        // Take the next button off-screen
         nextButtonTopAnchor?.constant = -80
     }
 
     private func showControls() {
+        
+        // Put the controls back onto the screen
         pageControlBottomAnchor?.constant = 16
         skipButtonTopAnchor?.constant = 16
         nextButtonTopAnchor?.constant = 16
     }
 }
 
-// MARK: - Actions
 extension OnboardingController {
 
     @objc func pageControlTapped(_ sender: UIPageControl) {
+        
+        /* If one of the page control dots are tapped, go to the respective
+           page */
+        
+        // Set the current page to the respective page tapped.
         setViewControllers([pages[sender.currentPage]], direction: .forward, animated: true, completion: nil)
+        
+        
+        // If necessary, animate the controls
         animateControlsIfNeeded()
     }
 
     @objc func skipTapped(_ sender: UIButton) {
+        
+        /* If the user presses the "finish" button, dismiss the tutorial */
         dismiss(animated: true, completion: nil)
 
     }
     
     @objc func nextTapped(_ sender: UIButton) {
+        
+        /* If the user presses the "next" button, go to the next page*/
         pageControl.currentPage += 1
         goToNextPage()
         animateControlsIfNeeded()
     }
 }
 
-// MARK: - Extensions
 extension UIPageViewController {
 
     func goToNextPage(animated: Bool = true, completion: ((Bool) -> Void)? = nil) {
+        
+        /* Set the next page as the view controller, if applicable*/
+        
+        // Fetch the current page
         guard let currentPage = viewControllers?[0] else { return }
+        
+        
+        /* Get the next page given the current page, and return nothing if
+           there is no proper next page */
         guard let nextPage = dataSource?.pageViewController(self, viewControllerAfter: currentPage) else { return }
         
+        
+        // Set the next page as the current page
         setViewControllers([nextPage], direction: .forward, animated: animated, completion: completion)
     }
     
     func goToPreviousPage(animated: Bool = true, completion: ((Bool) -> Void)? = nil) {
+        
+        /* Set the previous page as the view controller, if applicable*/
+        
+        // Fetch the current page
         guard let currentPage = viewControllers?[0] else { return }
+        
+        
+        /* Get the last page given the current page, and return nothing if
+           there is no proper last page */
         guard let prevPage = dataSource?.pageViewController(self, viewControllerBefore: currentPage) else { return }
         
+        
+        // Set the last page as the current page
         setViewControllers([prevPage], direction: .forward, animated: animated, completion: completion)
-    }
-    
-    func goToSpecificPage(index: Int, ofViewControllers pages: [UIViewController]) {
-        setViewControllers([pages[index]], direction: .forward, animated: true, completion: nil)
     }
 }
--- ServePlusDraft/Controllers/Onboarding/OnboardingViewController.swift
+++ ServePlusDraft/Controllers/Onboarding/OnboardingViewController.swift
@@ -4,32 +4,57 @@
 //
 //  Created by Vikram Khandelwal on 10/5/21.
 //
+//  Template view controller for each page in the tutorial
 
 import Foundation
 import UIKit
 
 class OnboardingViewController: UIViewController {
     
+    // Initialize view layout for the page components
     let stackView = UIStackView()
     
+    
+    // Initialize container for image to be displayed on page
     let imageView = UIImageView()
+    
+    
+    // Initialize title and subtitle for each page
     let titleLabel = UILabel()
     let subtitleLabel = UILabel()
+    
+    
+    // initialize variable for storing the size of the image
     var imageSize: Double?
     
+    
+    // Cutsom initialization function
     init(imageName: String, titleText: String, subtitleText: String, imageSize: Double) {
         super.init(nibName: nil, bundle: nil)
+        
+        // Set the imageView's image to the specified image
         imageView.image = UIImage(named: imageName)
+        
+        
+        // Set the title and subtitle text
         titleLabel.text = titleText
         subtitleLabel.text = subtitleText
+
+        
+        // Pass along the image size
         self.imageSize = imageSize
     }
     
+    
+    // required function
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     override func viewDidLoad() {
+        
+        /* Style and layout the page (setup already done) */
+        
         super.viewDidLoad()
         style()
         layout()
@@ -39,32 +64,90 @@ class OnboardingViewController: UIViewController {
 extension OnboardingViewController {
     
     func style() {
-        view.backgroundColor = .white
+        
+        // Make the background color responsive to light/dark mode
+        view.backgroundColor = UIColor.black
+        
+        /* Stack View Settings */
+        
+        // Make the constraints apply properly
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        // Center the Stack View
         stackView.axis = .vertical
         stackView.alignment = .center
+        
+        
+        // Add some breathing room
         stackView.spacing = 20
         
+        
+        
+        /* Image View Settings */
+        
+        // Make the constraints apply properly
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        // Autoscale the image
         imageView.contentMode = .scaleAspectFit
         
+        
+        
+        /* Title Label Settings*/
+        
+        // Make the constraints apply properly
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        // Set the default font style
         titleLabel.font = UIFont.preferredFont(forTextStyle: .title1)
         
+        
+        // Make the title color responsive to light/dark mode
+        titleLabel.textColor = .label
+
+        
+        
+        
+        /* Subtitle Label Settings */
+        
+        // Make the constraints apply properly
         subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        // Set the default font style
         subtitleLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        
+        
+        // Center the text inside the text container
         subtitleLabel.textAlignment = .center
         
+        
+        // Make the title color responsive to light/dark mode
+        subtitleLabel.textColor = .label
+
+        
+        // Make sure there is no clipping of text
         subtitleLabel.numberOfLines = 0
     }
         
     func layout() {
+        
+        /* Put the image and labels in the right place */
+        
+        // Add the image and labels to the stack view
         stackView.addArrangedSubview(imageView)
         stackView.addArrangedSubview(titleLabel)
         stackView.addArrangedSubview(subtitleLabel)
         
+        
+        // Add the stack view to the main view
         view.addSubview(stackView)
         
+        
+        // Position the stack view, images and labels
         NSLayoutConstraint.activate([
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
