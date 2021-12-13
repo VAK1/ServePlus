0da1ee2f6a0b442975fa168e37fcf93162e7dd7f
Vikram Khandelwal <vikram.a.khandelwal@gmail.com>
2021-12-12 17:20:58 -0800

    Commented the Tab Bar Controller

--- ServePlusDraft/Controllers/TabBarController.swift
+++ ServePlusDraft/Controllers/TabBarController.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Vikram Khandelwal on 8/4/21.
 //
+//  Tab Bar controller for navigating between the recording tab,
+//  the review tab and the analysis tab.
 
 import UIKit
 
@@ -13,18 +15,5 @@ class TabBarController: UITabBarController {
         super.viewDidLoad()
         self.selectedIndex = 1
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
